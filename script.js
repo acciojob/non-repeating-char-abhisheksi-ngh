676970d369c3672a55e6fb72f2a99e9cfe6d5769
A <abhishekkumarsingh9585@gmail.com>
2025-03-30 23:14:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+ let fre={};
+ for(let c of str)
+ {
+  fre[c]=(fre[c]||0)+1;
+ }
+ for(let ch of str){
+  if(fre[ch]===1){
+   return ch;
+  }
+ }
+ return null;
+ 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
